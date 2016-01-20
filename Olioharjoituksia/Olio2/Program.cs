a4170cde8f7fe692c442cf3a9f674e6406b7303a
Theleiba <henri.pollari@gmail.com>
2016-01-20 17:30:29 +0200

    Olio2 lisätty

---
+++ Olioharjoituksia/Olio2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio2
+{
+    class Car
+    {
+        public string ModelProperty
+        {
+            get { return model; }
+            set
+            {
+                if (value == "corolla" || value == "avensis")
+                {
+                    model = value;
+                }
+                else
+                {
+                    model = "unknown";
+                }
+            }
+        }
+        //class private members - DO NOT TOUCH, unless you know what you are doing
+        private string model;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car toyota = new Car();
+            toyota.ModelProperty = "911";
+
+            Console.WriteLine("This toyota model is: " + toyota.ModelProperty);
+            Console.ReadLine();
+        }
+    }
+}
