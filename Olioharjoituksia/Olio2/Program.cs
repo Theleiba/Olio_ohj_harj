4339407dde8bfdc41da9c47d89e8844eeb8cc354
Theleiba <henri.pollari@gmail.com>
2016-01-20 17:59:02 +0200

    Olio2 päivitys

--- Olioharjoituksia/Olio2/Program.cs
+++ Olioharjoituksia/Olio2/Program.cs
@@ -8,32 +8,32 @@ namespace Olio2
 {
     class Car
     {
+        public Car(string model, int cc)
+        {
+            this.model = model;
+            this.cc = cc;
+        }
+        public int EngineSize
+        {
+            get { return cc; }
+        }
         public string ModelProperty
         {
             get { return model; }
-            set
-            {
-                if (value == "corolla" || value == "avensis")
-                {
-                    model = value;
-                }
-                else
-                {
-                    model = "unknown";
-                }
-            }
+            set { model = value; }
         }
-        //class private members - DO NOT TOUCH, unless you know what you are doing
+        //class private members - DO NOT TOUCH, unless you know what u are doing
         private string model;
+        private readonly int cc;
+        private const int maxSpeed = 10;
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Car toyota = new Car();
-            toyota.ModelProperty = "911";
-
+            Car toyota = new Car("avensis", 1895);
             Console.WriteLine("This toyota model is: " + toyota.ModelProperty);
+            Console.WriteLine("This toyota engine size is: " + toyota.EngineSize);
             Console.ReadLine();
         }
     }
