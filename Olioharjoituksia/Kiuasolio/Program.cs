468dc7dbb9dd532a067960bc1d5efd2410354615
Theleiba <henri.pollari@gmail.com>
2016-01-20 19:45:46 +0200

    Kiuasolio & muutoksia muihin.

---
+++ Olioharjoituksia/Kiuasolio/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiuasolio
+{
+    class Kiuas
+    {
+        public bool Power()
+        {
+            return power = false;
+        }
+        public int Humid()
+        {
+            return humid;
+        }
+        public int Therm
+        {
+            get { return therm; }
+            set {
+                if (value > 0 && value <= 120)
+                {
+                    therm = value;
+                }
+                else therm = 0;
+            }
+        }
+        public int humid;
+        public int therm;
+        public bool power;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kiuas soona9000 = new Kiuas();
+            Console.WriteLine("Paina nappia kiukaan käynnistämiseksi.");
+            Console.ReadLine();
+            soona9000.power = true;
+            soona9000.therm = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna kosteuden arvo ");
+            soona9000.humid = int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ Olioharjoituksia/Olio3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olio3
+{
+    /*struct*/ class Point
+    {
+        public int x;
+        public int y;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point coordinate = new Point();
+            coordinate.x = 100;
+            coordinate.y = 50;
+
+            ModifyPoint(coordinate);
+
+            Console.WriteLine("coordinate is: " + coordinate.x + "," + coordinate.y);
+            Console.ReadLine();
+        }
+
+        static void ModifyPoint(Point p)
+        {
+            p.x += 500;
+            p.y += 1500;
+        }
+    }
+}
