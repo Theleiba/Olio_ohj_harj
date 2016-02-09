378fb98c333ea0ff9d06babd50387951f48fb33e
Theleiba <henri.pollari@gmail.com>
2016-02-09 18:39:08 +0200

    Demo 8 testiharjoituksia

---
+++ Olioharjoituksia/Demo8/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo8
+{
+    public class Calculator : ICalculator
+    {
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+        public int Divide(int number1, int number2)
+        {
+            return number1 / number2;
+        }
+    }
+}
